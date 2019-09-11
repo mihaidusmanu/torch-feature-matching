2a266d926eb711c53f68337463e53be2eaff64e2
Mihai Dusmanu <mihai.dusmanu@gmail.com>
2019-09-11 14:09:05 +0200

    Initial commit.

---
+++ feature_matchers.py
@@ -0,0 +1,57 @@
+import torch
+
+
+def mutual_nn_matcher(descriptors1, descriptors2):
+    # Mutual nearest neighbors (NN) matcher for L2 normalized descriptors.
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+    nn_sim, nn12 = torch.max(sim, dim=1)
+    nn_dist = torch.sqrt(2 - 2 * nn_sim)
+    nn21 = torch.max(sim, dim=0)[1]
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    mask = (ids1 == nn21[nn12])
+    matches = torch.stack([ids1[mask], nn12[mask]])
+    return matches.t(), nn_dist[mask]
+
+
+def ratio_matcher(descriptors1, descriptors2, ratio=0.8):
+    # Lowe's ratio matcher for L2 normalized descriptors.
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+    nns_sim, nns = torch.topk(sim, 2, dim=1)
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    matches = torch.stack([ids1, nns[:, 0]])
+    ratios = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    mask = (ratios <= ratio)
+    matches = matches[:, mask]
+    return matches.t(), nns_dist[mask, 0]
+
+
+def ratio_mutual_nn_matcher(descriptors1, descriptors2, ratio=0.8):
+    # Lowe's ratio matcher + mutual NN for L2 normalized descriptors.
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+    nns_sim, nns = torch.topk(sim, 2, dim=1)
+    nn12 = nns[:, 0]
+    nns_dist = torch.sqrt(2 - 2 * nns_sim)
+    nn21 = torch.max(sim, dim=0)[1]
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    matches = torch.stack([ids1, nns[:, 0]])
+    ratios = nns_dist[:, 0] / (nns_dist[:, 1] + 1e-8)
+    mask = torch.min(ids1 == nn21[nn12], ratios <= ratio)
+    matches = matches[:, mask]
+    return matches.t(), nns_dist[mask, 0]
+
+
+def similarity_matcher(descriptors1, descriptors2, threshold=0.7):
+    # Similarity threshold matcher for L2 normalized descriptors.
+    device = descriptors1.device
+    sim = descriptors1 @ descriptors2.t()
+    nn_sim, nn12 = torch.max(sim, dim=1)
+    nn_dist = torch.sqrt(2 - 2 * nn_sim)
+    nn21 = torch.max(sim, dim=0)[1]
+    ids1 = torch.arange(0, sim.shape[0], device=device)
+    mask = torch.min(ids1 == nn21[nn12], match_sim >= threshold)
+    matches = torch.stack([ids1[mask], nn12[mask]])
+    return matches.t(), nn_dist[mask]
